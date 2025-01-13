e0f514f92c164f33993b1aa22073d2a394698a68
jamsilva <jamsilva@DESKTOP-HKT2P0B>
2025-01-13 00:52:40 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoBiblioteca/Livro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciamentoBiblioteca
+{
+    internal class Livro
+    {
+        string titulo;
+        Autor autor;
+        int anoPublicacao;
+        int quantidadeExemplares;
+        int quantidadeDisponivel;
+
+        public Livro(string titulo, Autor autor, int anoPublicacao, int quantidadeExemplares)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anoPublicacao = anoPublicacao;
+            this.quantidadeExemplares = quantidadeExemplares;
+            this.quantidadeDisponivel = quantidadeExemplares;
+        }
+
+        public void Emprestar()
+        {
+            
+        }
+
+        public void Devolver()
+        {
+
+        }
+
+        public void VerificarDisponibilidade()
+        {
+
+        }
+    }
+}
